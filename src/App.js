5a49e2cefb12316da3c56a53ae996fec4a67c1e8
lekshmi12345 <lekshmisnair213@gmail>
2022-05-16 22:32:57 +0530

    Searchstudent updated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addstudent from './Component/Addstudent';
+import Searchstudent from './Component/Searchstudent';
 
 function App() {
   return (
     <>
-    <Addstudent/>
+    
+    <Searchstudent/>
     </>
   );
 }
