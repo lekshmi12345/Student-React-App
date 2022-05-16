f0c235d411d223a92787eeb48b9587a0fe15a58a
lekshmi12345 <lekshmisnair213@gmail>
2022-05-16 22:06:25 +0530

    updated addstudent

---
+++ src/Component/Addstudent.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Addstudent = () => {
+  return (
+    <div className="container">
+    <div className="row">
+        <h1>ADD STUDENT</h1>
+        <div className="clo col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            
+                <div className="row g-3">
+                    <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 ">
+                        <label form="" className="form-label">Name</label>
+                        <input type="text" className="form-control"/>
+                        
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="form-label">Roll no.</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label form="" className="form-label">Admno.</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label form="" className="form-label">university Regno.</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label for="" className="form-label">college</label>
+                        <input type="text" className="form-control"/>
+                    </div>
+                    <div className="col.col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 ">
+                        <button className="btn btn-success">REGISTER</button>
+
+                    </div>
+                    
+            
+                </div>
+            </div>
+        </div>
+    </div>
+
+  )
+}
+
+export default Addstudent
